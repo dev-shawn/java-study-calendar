742a1cb0f8f1e3c7d4cd5e98744a931d335343b8
audbang <audbang@218.38.137.27>
2020-12-06 22:31:48 +0900

    Extract Method

--- src/study/calendar/Calendar.java
+++ src/study/calendar/Calendar.java
@@ -3,8 +3,13 @@ package study.calendar;
 import java.util.Scanner;
 
 public class Calendar {
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-	public static void main(String[] args) {
+	public int getMaxDaysOfMonth(int month) {
+		return MAX_DAYS[month - 1];
+	}
+
+	public void printSampleCalendar() {
 		System.out.println(" Hello, Calendar");
 		System.out.println(" ----------------------");
 		System.out.println(" 일  월  화  수  목  금  토");
@@ -13,15 +18,18 @@ public class Calendar {
 		System.out.println(" 8  9 10 11  12  13  14");
 		System.out.println("15 16 17 18  19  20  21");
 		System.out.println("22 23 24 25  26  27  28\n");
+	}
+
+	public static void main(String[] args) {
 
 		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
 		System.out.println("최대 일수를 보고자 하는 달을 입력하시오.");
 		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
+		Calendar cal = new Calendar();
+		int month = sc.nextInt();
 
-		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-		System.out.printf("%d 월은 %d일 까지 있습니다.", a , maxDays[a-1]);
+		System.out.printf("%d 월은 %d일 까지 있습니다.", month, cal.getMaxDaysOfMonth(month));
 		sc.close();
-	} 
+	}
 
 }
